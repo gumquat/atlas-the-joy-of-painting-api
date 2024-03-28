b8ed325afbb94d97aab771aacf0f0a10b2c6ec68
gumquat <gumquat@gmail.com>
2024-03-28 13:36:08 -0500

    push

---
+++ pandaTest.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+print(pd.__version__)
+print(pd.__file__)
+
+import sys 
+print(sys.executable)
--- sanitizeOriginal.py
+++ sanitizeOriginal.py
@@ -1,25 +1,25 @@
-import psycopg2
+# import psycopg2
 import csv
+import pandas as pandas
 
     # CONNECT TO DATBASE
-conn = psycopg2.connect(
-    dbname="the-joy-of-painting",
-    user="postgres",
-    password="Marxist123!",
-    host="localhost",
-    port="5432"
-)
+# conn = psycopg2.connect(
+#     dbname="the-joy-of-painting",
+#     user="postgres",
+#     password="Marxist123!",
+#     host="localhost",
+#     port="5432"
+# )
 # Create a cursor object to execute SQL queries
-cur = conn.cursor()
+# cur = conn.cursor()
 
-    # PREPARE DATA LISTS
+    # PREPARE DATA LISTS to be written into
 episode_id = [] # episode ID
 painting_title = []  # Your list of titles
 colors = [] # all colors given unique ID
 hexValue = [] # all colors by hex
 colors_used = [] # colors used in episodes by id
 
-    # OPEN THE CSV FILE
     # Open the input CSV file in read mode
 with open('dirtyData/colorsUsed.csv', 'r') as infile:
     # Create a CSV reader object
@@ -34,13 +34,15 @@ with open('dirtyData/colorsUsed.csv', 'r') as infile:
         for row in reader:
             # Edit the row (e.g., add an extra column)
             edited_row = row + ['new_column_data']
+            # TO DO: give each color from colorsUsed an ID number
+            # for each episode in the new csv file, create an array containing used color's ID numbers
 
             # Write the edited row to the output CSV file
             writer.writerow(edited_row)
 
     # COMMIT the transaction
-conn.commit()
+#conn.commit()
 
     # CLOSE IT ALL OUT
-cur.close()
-conn.close()
+# cur.close()
+# conn.close()
