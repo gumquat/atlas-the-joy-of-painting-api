553561fc54831e16d6176e037de704806cfcccd4
gumquat <gumquat@gmail.com>
2024-04-01 15:55:31 -0500

    push to stay updated

--- db_connectPush.py
+++ db_connectPush.py
@@ -1,43 +1,31 @@
-# import psycopg2
 import csv
-import pandas as pandas
+# import pandas as pandas
+import psycopg2
+
+#/var/lib/postgresql/14/main
 
     # CONNECT TO DATBASE
 conn = psycopg2.connect(
-    dbname="the-joy-of-painting",
+    dbname="DATABASE_paintings",
     user="postgres",
-    password="Marxist123!",
+    password="Password123", # IT SAYS THIS IS INCORRECT????? <---
     host="localhost",
     port="5432"
 )
 # Create a cursor object to execute SQL queries
 cur = conn.cursor()
 
-### BEGIN WRITING DATA TO SQLSERVER ### ### ###
 # change 'data'csv' with the path to your csv file
-# Open the CSV file
-with open('data.csv', 'r') as csvfile:
-    csvreader = csv.reader(csvfile)
-
-    # Skip the header row
-    next(csvreader)
-
-    # Prepare the SQL statement
-    # note: run 'generate_insert_statement' on your csv file first,
-    # then place the generated SQL commands here
-    insert_query = """
-        PLACE YOUR INSERT TEXT HERE
-    """
-
-    # Iterate over the rows in the CSV file
-    for row in csvreader:
-        # Execute the SQL statement
-        cur.execute(insert_query, row)
-### END WRITING DATA TO SQLSERVER ### ### ###
+# cleanData/colors.csv
+# cleanData/episode_colors.csv
+# cleanData/episode_subjects.csv
+# cleanData/episodes.csv
+# cleanData/subjects.csv
+with open('cleanData/colors.csv', 'r') as csvfile:
+    cur.copy_expert("COPY mytable FROM STDIN WITH CSV HEADER DELIMITER ','", csvfile)
 
     # COMMIT the transaction
 conn.commit()
-
     # CLOSE IT ALL OUT
 cur.close()
 conn.close()
