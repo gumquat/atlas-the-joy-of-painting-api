3cbabfbdf551e6e05e7c85dfd41f139b48c77927
gumquat <gumquat@gmail.com>
2024-04-01 16:55:10 -0500

    SO CLOSE

--- db_connectPush.py
+++ db_connectPush.py
@@ -1,6 +1,7 @@
 # import pandas as pandas
 import csv
 import psycopg2
+from psycopg2 import sql
 
 #/var/lib/postgresql/14/main
 
@@ -15,20 +16,44 @@ conn = psycopg2.connect(
 # Create a cursor object to execute SQL queries
 cur = conn.cursor()
 
-# change 'data'csv' with the path to your csv file
-with open('cleanData/colors.csv', 'r') as csvfile:
-    cur.copy_expert("COPY mytable FROM STDIN WITH CSV HEADER DELIMITER ','", csvfile)
+# Check if the table exists
+table_exists_query = """
+    SELECT EXISTS (
+        SELECT 1
+        FROM   information_schema.tables 
+        WHERE  table_name = 'colors'
+    )
+"""
+try:
+    cur.execute(table_exists_query)
+    table_exists = cur.fetchone()[0]
+    if not table_exists:
+        # Create the table
+        create_table_query = """
+            CREATE TABLE colors (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(50) NOT NULL,
+                hex_value VARCHAR(7) NOT NULL
+            )
+        """
+        cur.execute(create_table_query)
+        # Commit changes
+        conn.commit()
+        print("Table 'colors' created successfully!")
+    else:
+        print("Table 'colors' already exists.")
+except psycopg2.Error as e:
+    print("Error:", e)
 
-    # COMMIT the transaction
-conn.commit()
+# THIS CODE PUSHES DATA FROM CSV FILE TO THE TABLES NAMED IN ITS ARGUMENTS
+# change 'data'csv' with the path to your csv file
+# with open('cleanData/colors.csv', 'r') as csvfile:
+#     cur.copy_expert("COPY mytable FROM STDIN WITH CSV HEADER DELIMITER ','", csvfile)
+
     # CLOSE IT ALL OUT
 cur.close()
 conn.close()
 
 ### PICK UP FROM WHERE I LEFT OFF HERE
-#python3 db_connectPush.py
-# Traceback (most recent call last):
-#   File "/mnt/c/Users/Evan Newman/Documents/GitHub/atlas-the-joy-of-painting-api/db_connectPush.py", line 25, in <module>
-#     cur.copy_expert("COPY colors FROM STDIN WITH CSV HEADER DELIMITER ','", csvfile)
-# psycopg2.errors.UndefinedTable: relation "colors" does 
-# not exist
+# https://chat.openai.com/share/5931f10b-cd20-4ba2-af98-11ee3ba32ba9
+# https://chat.openai.com/share/417d17a5-9c90-465f-80a6-d8694fd00244
