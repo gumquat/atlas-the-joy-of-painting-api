7f510704ffe06d947cedcc6bca852caef8c850bd
gumquat <gumquat@gmail.com>
2024-04-03 16:18:42 -0500

    dying inside

--- pushData.py
+++ pushData.py
@@ -16,6 +16,7 @@ conn = psycopg2.connect(
 # Create a cursor object to execute SQL queries
 cur = conn.cursor()
 
+### INSERT INTO COLORS, SUBJECTS, EPISODES ###
 # Name of the table to insert data into
 table_name = "episode_colors"
 # Open the CSV file
@@ -35,6 +36,23 @@ with open("cleanData/episode_colors.csv", "r") as csv_file:
 
         # Execute the INSERT statement
         cur.execute(insert_query, row)
+
+# ## INSERT INTO EPISODE_COLORS and EPISODE_SUBJECTS ###
+# table_name = "episode_colors"
+# with open("cleanData/episode_colors.csv", "r") as csv_file:
+#     csv_reader = csv.reader(csv_file)
+
+#     next(csv_reader)
+
+#     for row in csv_reader:
+#         episode_id = row[0]
+
+#         color_ids = [int(x) for x in row[1].split(',')]
+
+#         for color_id in color_ids:
+#             insert_query = "INSERT INTO {} (episodeID, colorID) VALUES (%s, %s)".format(table_name)
+
+#             cur.execute(insert_query, (episode_id, color_id))
 
 # Commit the changes and close the connection
 conn.commit()
